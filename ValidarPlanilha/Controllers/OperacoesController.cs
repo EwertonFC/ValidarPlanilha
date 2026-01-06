f1be7ca66a5638a09ba9e8dbda1dc6cfa42deed1
ewerton.costa <ewerton.costa@LOTNOT97>
2026-01-06 12:04:38 -0300

    Adicionar arquivos de projeto.

---
+++ ValidarPlanilha/Controllers/OperacoesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ValidarPlanilha.Controllers
+{
+
+    [ApiController]
+    [Route("api/operacoes")]
+    public class OperacoesController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public IActionResult Upload(IFormFile arquivo)
+        {
+            if(arquivo == null)
+            {
+                return BadRequest(new
+                {
+                    mensagem = "Nenhum arquivo recebido"
+                });
+            }
+            if(arquivo.Length == 0)
+            {
+                return BadRequest("Arquivo menor do que esperado");
+            }
+            return Ok(new 
+            {
+                dataRecebimento = DateTime.UtcNow,
+                nomeArquivo = arquivo.FileName,
+                tamanho = arquivo.Length
+            });
+        }
+    }
+}
